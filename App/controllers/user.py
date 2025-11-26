2f219ecb32291fccf1a06b08a21a6a9cf24fffd2
SincerelyKM <keddishamcintyre@gmail.com>
2025-11-26 15:17:19 -0400

    Fixed the controllers

--- App/controllers/user.py
+++ App/controllers/user.py
@@ -1,53 +1,32 @@
+
 from App.models import User, Student, Employer, Staff
 from App.database import db
 
-def create_user(username, password, user_type):
-    try:
-        newuser = User(username=username, password=password, role=user_type)
-        db.session.add(newuser)
-        db.session.flush() 
-        
-        if user_type == "student":
-            student = Student(username=username, user_id=newuser.id)
-            db.session.add(student)
-        elif user_type == "employer":
-            employer = Employer(username=username, user_id=newuser.id)
-            db.session.add(employer)
-        elif user_type == "staff":
-            staff = Staff(username=username, user_id=newuser.id)
-            db.session.add(staff)
-        else:
-            return False
-        
-        db.session.commit()
-        return True
-    except Exception as e:
-        db.session.rollback()
+def create_user(username, password , role = "user"):
+    newUser =  User (username=username, password = password)
+    newUser.Role = role
+    db.session.add(newUser)
+    db.session.commit()
+    return newUser
+
+def login( username, password):
+    user = User.query.filter_by(username =username).first()
+    if user and user.check_password(password):
+        return user
+    return None 
+
+def check_password( userID, password):
+    user= User.query.get(userID)
+    if not user: 
         return False
+    return user.check_password(password)
+
+def set_password( userID, newPassword):
+    user = User.query.get(userID)
+    if not user:
+        return None
+    user.set_password(newPassword)
+    db.session.commit()
+    return user
 
 
-def get_user_by_username(username):
-    result = db.session.execute(db.select(User).filter_by(username=username))
-    return result.scalar_one_or_none()
-
-def get_user(id):
-    return db.session.get(User, id)
-
-def get_all_users():
-    return db.session.scalars(db.select(User)).all()
-
-def get_all_users_json():
-    users = get_all_users()
-    if not users:
-        return []
-    users = [user.get_json() for user in users]
-    return users
-
-def update_user(id, username):
-    user = get_user(id)
-    if user:
-        user.username = username
-        # user is already in the session; no need to re-add
-        db.session.commit()
-        return True
-    return None
